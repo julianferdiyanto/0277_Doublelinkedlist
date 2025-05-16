cc5921873260dcbb6e22e3649c58d342fdcfe16f
julianferdiyanto <julianferdiyanto3@gmail.com>
2025-05-16 10:08:56 +0700

    menambahkan prosedure int main

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -211,3 +211,52 @@ public:
         }
     }
 };
+
+int main()
+{
+    DoubleLinkedList list;
+    char choice;
+
+    do
+    {
+
+        cout << "\nMenu\n";
+        cout << "\n1. Add Record\n";
+        cout << "\n2. Delete Record";
+        cout << "\n3. View all Records (Ascending)\n";
+        cout << "\n4. View all Records (Descending)\n";
+        cout << "\n5. Search Record\n";
+        cout << "\n6. Exit\n";
+        cout << "\nEnter your choice: ";
+
+        cin >> choice;
+
+        switch (choice)
+        {
+        case 1:
+            list.addNode();
+            break;
+        case 2:
+            list.hapus();
+            break;
+        case 3:
+            list.traverse();
+            break;
+        case 4:
+            list.revtraverse();
+            break;
+        case 5:
+            list.searchData();
+            break;
+        case 6:
+            return 0;
+        default:
+            cout << "\nInvalid option\n";
+        }
+        cout << "\nPress Enter to continue...";
+        cin.ignore();
+        cin.get();
+        cout << endl;
+        system("clear");
+    } while (choice != 6);
+}
