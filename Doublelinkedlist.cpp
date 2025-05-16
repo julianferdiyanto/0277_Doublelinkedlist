65c744c8bb39b26a7ea94d90bf3e54acf0efac1f
julianferdiyanto <julianferdiyanto3@gmail.com>
2025-05-16 09:54:04 +0700

    menambahkan node baru

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -20,4 +20,11 @@ public:
     {
         START = NULL;
     }
+
+    void addNode()
+    {
+        int nim;
+        cout << "\nEnter the roll number of the student: ";
+        cin >> nim;
+    }
 }
